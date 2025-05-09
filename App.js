cc9c0ae90f241a3fc82c9e020d3f5f2a7968f6f8
JULIETA WAINHAUS BARBER <48460067@ort>
2025-05-09 10:38:39 -0300

    falta terminar

--- App.js
+++ App.js
@@ -1,12 +1,44 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { SafeAreaView, ImageBackground, StyleSheet, Text, View, TextInput, TouchableOpacity, Pressable } from 'react-native';
 
 export default function App() {
+  const imagen=''; //RUTA DE LA IMAGEN
+  const [nombre, handleNombreChange] = React.useState('');
+  const botonPresionado = () => styles.boton= backgroundColor='blue';
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    
+    <SafeAreaView style = {styles.container} >
+    <View>
+      <StatusBar style={styles.statusbar} />
+      
+      <ImageBackground 
+        source={imagen}
+        resizeMode='cover'
+        style={styles.foto}>
+      </ImageBackground>
+
+      <Text style={styles.nombre}>Vera Romanowicz</Text>
+      <Text style={styles.titulo}>Desarrolladora frontend</Text>
+
+      <TextInput
+        style={styles.input}
+        placeholder='Enter a message'
+        onChangeText={handleNombreChange}
+        value={nombre}
+      />
+      
+      <TouchableOpacity style={styles.boton} onPress={() => Alert.alert("Mensaje enviado")}>
+        <Text>Contactar</Text>
+      </TouchableOpacity>
+
+      <Pressable style={styles.botonSecundario}
+        onPress={botonPresionado}>
+        <Text>Ver Perfil</Text>
+      </Pressable>
+      
     </View>
+    </SafeAreaView>
   );
 }
 
@@ -17,4 +49,15 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+
+  foto: {},
+
+  statusbar: {
+    backgroundColor: '#f5a6ca',
+  },
+
+  boton: {
+    backgroundColor: 'red',
+  }
+
 });
